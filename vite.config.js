1c1e4989841c8f0fdc040d8e4986927fadac6239
Plastic Digits <plasticdigits@protonmail.com>
2024-08-15 03:50:31 +1000

    fix broken build by removing vite-plugin-radar import from config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
